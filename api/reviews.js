44d0433d46bb01533b77b1c9ff260eb4f04970fc
Parker-Pond <parker.a.pond@gmail.com>
2023-06-28 11:29:27 -0600

    updated API in class dosent work yet

--- api/reviews.js
+++ api/reviews.js
@@ -19,4 +19,22 @@ reviewsRouter.get("/", async (request, response) => {
   }
 });
 
+reviewsRouter.post("/post", async (req, res)=>{
+  consol.log(" entered into reviews Router", req)
+try {
+  const {rows} = await client.query(`
+  INSERT INTO reviews("ideaName", title, author, review, rating)
+  VALUES ($1, $2, $3, $4, $5)
+  RETURNING *;`
+  ,[reviewObj.ideaName, reviewObj.title, reviewObj.author, reviewObj.review, reviewObj.rating])
+
+  return rows
+  
+} catch (error) {
+  console.log("error posting to the Reviews table", error)
+}
+
+
+})
+
 module.exports = reviewsRouter;
