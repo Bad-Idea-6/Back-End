3cfa1df426b53d8b8f3bcdb3c63704bbf00f7340
Parker-Pond <parker.a.pond@gmail.com>
2023-06-30 11:52:20 -0600

    Login function is now working

--- db/userFinder.js
+++ db/userFinder.js
@@ -1,18 +1,21 @@
-const client = require("./client")
-
+const client = require("./client");
 
 //? this function need to go through the database by the username then gives us al reletive data conected to the user.
 
-async function findUserByUsername (userNameValue){
-    try {
-        const {rows} = await client.query(`
-            SELECT FROM username
-            WHERE username = ${userNameValue};
-        `)
-        return rows
-    } catch (error) {
-        console.log("userFinder ERROR", error)
-    }
+async function findUserByUsername(userNameValue) {
+  try {
+    const { rows } = await client.query(
+      `
+            SELECT * FROM users
+            WHERE "username" = $1;
+        `,
+      [userNameValue]
+    );
+
+    return rows[0];
+  } catch (error) {
+    console.log("userFinder ERROR", error);
+  }
 }
 
-module.exports ={findUserByUsername}
+module.exports = { findUserByUsername };
