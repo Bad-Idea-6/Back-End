cf010ad417932eef269db1bf0b7e0435f81e4aaf
doublehelix4l <jakercrockett@gmail.com>
2023-07-05 14:09:23 -0500

    got register/login to work

--- db/users.js
+++ db/users.js
@@ -1,12 +1,12 @@
 const client = require("./client")
 
-async function createNewUser( {firstName, lastName, username, password, is_admin}){
+async function createNewUser( {firstName, lastName, username, password, is_admin, email}){
 try {
     const{rows} = await client.query(`
-    INSERT INTO users( "firstName", "lastName", username, password, is_admin)
-    VALUES ($1, $2, $3, $4, $5)
+    INSERT INTO users( "firstName", "lastName", username, password, is_admin, email)
+    VALUES ($1, $2, $3, $4, $5, $6)
     RETURNING *;`
-    ,[firstName, lastName, username, password, is_admin])
+    ,[firstName, lastName, username, password, is_admin, email])
     console.log("user created")
     return rows
 } catch (error) {
