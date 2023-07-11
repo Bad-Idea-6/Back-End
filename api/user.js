417b340daef80239e7106ad53c98207c8094ba74
Parker-Pond <parker.a.pond@gmail.com>
2023-07-11 12:56:19 -0600

    got patch request for users and reviews

--- api/user.js
+++ api/user.js
@@ -3,32 +3,33 @@ const usersRouter = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
-const { createNewUser } = require("../db/users");
+const { createNewUser, updateUser } = require("../db/users");
 const { findUserByUsername, findUserById } = require("../db/userFinder");
+const passwordHasher = require("../db/passwordHasher")
 
 // TODO: REGISTER NEW USER HERE
 
 usersRouter.post("/register", async (req, res) => {
   const { firstName, lastName, username, password, email } = req.body;
-  console.log(req.body, "34234234234234234")
+  console.log(req.body, "34234234234234234");
   try {
     //? this below checks if the username already exists
-    const _user = await findUserByUsername(username)
+    const _user = await findUserByUsername(username);
     if (!_user) {
-      const saltRound = await bcrypt.genSalt(8)
-      const myHashedPassword = await bcrypt.hash(password, saltRound)
-      console.log(myHashedPassword, "myhashedpassword")
+      const saltRound = await bcrypt.genSalt(8);
+      const myHashedPassword = await bcrypt.hash(password, saltRound);
+      console.log(myHashedPassword, "myhashedpassword");
       const user = await createNewUser({
         firstName,
         lastName,
         username,
         password: myHashedPassword,
-        email
+        email,
       });
 
       const token = jwt.sign(
         {
-          username
+          username,
         },
         process.env.JWT_SECRET,
         { expiresIn: "1w" }
@@ -37,15 +38,13 @@ usersRouter.post("/register", async (req, res) => {
         message: "Thank you for joining the Bad Idea Cult.",
         token,
       });
-    }
-    else {
-      res.send("username already un use. please try again ")
+    } else {
+      res.send("username already un use. please try again ");
     }
   } catch (error) {
     console.log("error registering new user", error);
   }
 });
-
 
 // TODO: LOGIN AN EXISTING USER
 usersRouter.post("/login", async (req, res) => {
@@ -60,49 +59,73 @@ usersRouter.post("/login", async (req, res) => {
       const token = jwt.sign(
         {
           id: testedUsername.id,
-          username
+          username,
         },
 
         process.env.JWT_SECRET,
         { expiresIn: "1w" }
-
-      )
-console.log("you made it here! line 70")
+      );
+      console.log("you made it here! line 70");
       res.send({
         message: "you have logged in!!!",
-        token, success: true,
-        id:testedUsername.userId,
-        is_admin: testedUsername.is_admin
+        token,
+        success: true,
+        id: testedUsername.userId,
+        is_admin: testedUsername.is_admin,
       });
-    }
-    else {
-      console.log("line 78")
+    } else {
+      console.log("line 78");
       res.send({
         message: "you have NOT logged in!!!",
-        token: null, success: false
+        token: null,
+        success: false,
       });
-
     }
   } catch (error) {
     console.log("error during login sequence api/user.js", error);
   }
 });
 
-usersRouter.post("/profile", async(req, res)=>{
+usersRouter.post("/profile", async (req, res) => {
   try {
-    const {id} = req.body;
-    const user = await findUserById(id)
-    console.log(user)
-    if (user){
-      res.send(
-        user
-      )
+    const { id } = req.body;
+    const user = await findUserById(id);
+    console.log(user);
+    if (user) {
+      res.send(user);
     }
+  } catch (error) {}
+});
 
+usersRouter.patch("/update-user", async (req, res) => {
+  try {
+    const { id, firstName, lastName, username, password, email } = req.body;
+const fields= {}
+if(firstName){
+  fields.firstName = firstName
+}
+if(lastName){
+  fields.lastName = lastName
+}
+if(username){
+  fields.username = username
+}
+if(password){
+  const hashedPassword = await passwordHasher(password)
+  fields.password = hashedPassword
+}
+if(email){
+  fields.email = email
+}
 
+console.log(fields)
+
+    const updatedUser = await updateUser(id, fields);
+   console.log("got through the function")
+    res.send(updatedUser)
   } catch (error) {
-    
+    console.log("userRouter.Patch error", error)
   }
-})
+});
 
 module.exports = usersRouter;
--- db/users.js
+++ db/users.js
@@ -1,17 +1,57 @@
-const client = require("./client")
+const client = require("./client");
 
-async function createNewUser( {firstName, lastName, username, password, is_admin, email}){
-try {
-    const{rows} = await client.query(`
+async function createNewUser({
+  firstName,
+  lastName,
+  username,
+  password,
+  is_admin,
+  email,
+}) {
+  try {
+
+console.log(typeof is_admin, "is admin??????")
+    if (is_admin!== true && is_admin !== false){
+        is_admin = false
+    }
+
+
+    const { rows } = await client.query(
+      `
     INSERT INTO users( "firstName", "lastName", username, password, is_admin, email)
     VALUES ($1, $2, $3, $4, $5, $6)
-    RETURNING *;`
-    ,[firstName, lastName, username, password, is_admin, email])
-    console.log("user created")
-    return rows
-} catch (error) {
-    console.error("createNewUser error in users.js", error)
-}
+    RETURNING *;`,
+      [firstName, lastName, username, password, is_admin, email]
+    );
+    console.log("user created");
+    return rows;
+  } catch (error) {
+    console.error("createNewUser error in users.js", error);
+  }
 }
 
-module.exports = {createNewUser}
+async function updateUser(id, fields) {
+    const arrayOfKeys = Object.keys(fields);
+    console.log(arrayOfKeys)
+    const mapOfSetStringNames = arrayOfKeys.map((key, index) => {
+      return `"${key}"=$${index + 1}`;
+    });
+    console.log(mapOfSetStringNames)
+    const setString = mapOfSetStringNames.join(", ");
+console.log(setString)
+    if (setString.length === 0) {
+      return;
+    }
+  try {
+    const { rows } = await client.query(`
+    UPDATE users
+    SET ${setString}
+    WHERE "userId" = ${id}
+    RETURNING *;`,Object.values(fields));
+    console.log(rows[0], "!!!!!!!")
+    return rows[0];
+  } catch (error) {
+    console.log("done did screwed updid", error);
+  }
+}
+module.exports = { createNewUser, updateUser };
